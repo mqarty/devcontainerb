60d055a2767bd6eb017dbb4244aa7bb37002c99b
Marty Berryman <mqarty@gmail.com>
2025-11-13 00:52:28 +0000

    updates

--- .devcontainer/setup-git-safe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-# Ensure git is configured with safe settings
-git config --global init.defaultBranch main
-git config --global pull.rebase true
-git config --global --add safe.directory /workspaces/b6n/notebooks
-git config --global --add safe.directory /workspaces/b6n/devcontainerb
